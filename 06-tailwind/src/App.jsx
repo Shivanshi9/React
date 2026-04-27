3d30a2900d646073617b20abb4fd8bd1df26a8fa
Shivanshi9 <shivanshisugandhi@gmail.com>
2026-04-27 18:45:15 +0530

    tailwind installation

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-gray-900'>
+      {/* <h1 class="text-3xl font-bold underline">
+          Hello world!
+      </h1> */}
+    </div>
+  )
+}
+
+export default App
